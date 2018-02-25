d48e23a3395598d61011f10637d1415f571b98d8
huziy <guziy.sasha@gmail.com>
2018-02-24 22:48:47 -0500

    backup 24/02/2018

--- compare_multiple_simulations.py
+++ compare_multiple_simulations.py
@@ -1,3 +1,10 @@
+import calendar
+
+import matplotlib
+
+from util import plot_utils
+
+matplotlib.use("agg")
 from collections import OrderedDict
 
 # plot climatological means (for selected months) and a time series of area-averages (over land)
@@ -13,53 +20,83 @@ from rpn.rpn import RPN
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
+from matplotlib.colors import BoundaryNorm
 
 LAND_MASK = None
 BASEMAP = None
 LONS = None
 LATS = None
 
-def get_ym(month_folder: Path=None):
+# color levels
+vname_to_clevels = {
+    "PR": [0, 0.5, 1, 2, 3, 5, 10], 
+    "TT": [-20, -15, -10, -5, -2, 0, 2, 5, 10, 15, 20]
+}
+
+def get_ym(month_folder: Path = None):
     ym = month_folder.name.split("_")[-1]
+    print(f"ym={ym}; mf={month_folder}")
     return int(ym[:-2]), int(ym[-2:])
 
 
 def plot_diags(mean_field, area_avg_series, samples_dir_path: Path, label="", varname=""):
-
+    global BASEMAP
     fig = plt.figure()
-    
+
     gs = GridSpec(1, 2)
 
-    ax = fig.add_subplot(gs[0, 0])
-    xx, yy = BASEMAP(LONS, LATS)
-    im = BASEMAP.contourf(xx, yy, mean_field, 21, cmap="bwr", ax=ax)
-    BASEMAP.colorbar(im)
-
     ax = fig.add_subplot(gs[0, 1])
+    xx, yy = BASEMAP(LONS, LATS)
+    
+    clevs = vname_to_clevs[varname]
+    norm = BoundaryNorm(clevs, len(clevs) - 1) 
+    cmap = cm.get_cmap("bwr", len(clevs) - 1)
+    im = BASEMAP.contourf(xx, yy, mean_field, levels=clevs, norm=norm, cmap=cmap, ax=ax, extend="both")
+    BASEMAP.colorbar(im)
+    BASEMAP.drawcoastlines(ax=ax)
+    ax.set_xlabel(label)
+
+    ax = fig.add_subplot(gs[0, 0])
     keys = list(area_avg_series.keys())
     data = [area_avg_series[k] for k in keys]
     s = pd.Series(index=keys, data=data)
     s.sort_index(inplace=True)
     s.plot(ax=ax)
+    ax.set_title("area average over land")
+
 
     diag_plots = samples_dir_path.parent / "diag_plots"
     diag_plots.mkdir(parents=True, exist_ok=True)
-    img_file = diag_plots / f"{varname}_{label}.png"
-    fig.savefig(str(img_file), bbox_inches="tight", dpi=300)
-
+    label_fname_part = label.split(",")[0].replace("/", "_")
+    img_file = diag_plots / f"{varname}_{label_fname_part}.png"
+    fig.savefig(str(img_file), bbox_inches="tight", dpi=150)
 
 
 def main():
     global BASEMAP, LAND_MASK
     simlabel_to_path = OrderedDict([
-        ("NA044_pgi11code_pgicompiled(initial)", "/home/huziy/current_project/Output/test_compilers/NorthAmerica_0.44deg_testPGI_before_gfrefactorings/Samples"),
-        ("NA044_pgi11+gfortrancode_pgicompiled", "/home/huziy/current_project/Output/test_compilers/NorthAmerica_0.44deg_testPGI/Samples")
+        ("NA044_pgi11code_pgicompiled(initial)",
+         "/home/huziy/current_project/Output/test_compilers/NorthAmerica_0.44deg_testPGI_before_gfrefactorings/Samples"),
+        ("NA044_pgi11+gfortrancode_pgicompiled",
+         "/home/huziy/current_project/Output/test_compilers/NorthAmerica_0.44deg_testPGI/Samples"),
+        ("NorthAmerica_0.44deg_testgfortran_cedar",
+         "/home/huziy/current_project/Output/test_compilers/NorthAmerica_0.44deg_testgfortran_cedar/Samples")
     ])
 
-    varname = "PR"
-    filename_prefix = "pm"
-    months_of_interest = [1, 2, 12] # only for the 2d map
+    varname = "TT"
+
+    units_db = {
+        "PR": ["mm/day", 1000 * 24 * 3600],
+        "TT": ["C", 1],
+    }
+
+
+    filename_prefix = "dm"
+    months_of_interest = [1, 2, 12]  # only for the 2d map
+    units, multiplier = units_db[varname]
+    
+
+    plot_utils.apply_plot_params(font_size=10)
 
     for label, path in simlabel_to_path.items():
         path = Path(path)
@@ -69,36 +106,49 @@ def main():
                                                    months_list=months_of_interest, samples_folder_path=path)
 
         # do the plotting
-        plot_diags(mean_field, area_avg_series, samples_dir_path=path, label=label, varname=varname)
-
+        months_labels = ",".join([calendar.month_name[i][:2] for i in months_of_interest])
+        panel_label = f"{label}, {varname} ({units}), \n [months={months_labels}]"
+        mean_field *= multiplier
+        area_avg_series = {k: v * multiplier for k, v in area_avg_series.items()}
+        plot_diags(mean_field, area_avg_series, samples_dir_path=path, label=panel_label, varname=varname)
 
         # reset global vars to be reused for other simulations
         LAND_MASK = None
         BASEMAP = None
 
 
-def diagnose_var(varname="PR", fname_prefix="pm", months_list=None, samples_folder_path: Path=None):
-
+def diagnose_var(varname="PR", fname_prefix="pm", months_list=None, samples_folder_path: Path = None):
     area_avg_series = {}
     current_counter = 0
     current_mean_field = None
     for monthdir in samples_folder_path.iterdir():
-        mean_field, counter, area_avg = process_month(varname=varname, fname_prefix=fname_prefix,
-                                            months_list=months_list, month_folder_path=monthdir)
 
-        if current_mean_field is None:
-            current_mean_field = mean_field
-            current_counter = counter
-        else:
-            w = current_counter / (current_counter + counter)
-            current_mean_field = current_mean_field * w + mean_field * (1 - w)
+        # ignore .DS_Store
+        if monthdir.name.startswith("."):
+            continue
+
+        print(f" processing {monthdir}")
+        mean_field, counter, area_avg = process_month(varname=varname, fname_prefix=fname_prefix,
+                                                      months_list=months_list, month_folder_path=monthdir)
+
+        print(f"counter={counter}")
+
+        if counter > 0:
+            if current_mean_field is None:
+                current_mean_field = mean_field
+                current_counter = counter
+            else:
+                print(current_counter, counter)
+                w = current_counter / (current_counter + counter)
+                current_mean_field = current_mean_field * w + mean_field * (1 - w)
+                current_counter += counter
 
         area_avg_series.update(area_avg)
-        
+
     return current_mean_field, area_avg_series
 
 
-def process_month(varname="PR", fname_prefix="pm", months_list=None, month_folder_path: Path=None):
+def process_month(varname="PR", fname_prefix="pm", months_list=None, month_folder_path: Path = None):
     global BASEMAP, LONS, LATS, LAND_MASK
     year, month = get_ym(month_folder_path)
 
@@ -113,8 +163,24 @@ def process_month(varname="PR", fname_prefix="pm", months_list=None, month_folde
             continue
 
         with RPN(str(afile)) as r:
+
+            if varname not in r.variables:
+                print(f"{afile} does not contain {varname}, skipping ...")
+                continue
+
+            print(f"Processing {afile}")
+
             field = r.variables[varname][:].squeeze()
-            fields.append(field)
+
+            print(field.shape)
+
+            if field.ndim == 2:
+                fields.append(field)
+            elif field.ndim == 3:
+                fields.extend([f for f in field])
+            else:
+                raise ValueError(
+                    f"Can only handle 2d (x, y) and 3d (t, x, y) fields, but got field.shape={field.shape}")
 
             if BASEMAP is None:
                 LONS, LATS = r.get_longitudes_and_latitudes_for_the_last_read_rec()
@@ -128,7 +194,10 @@ def process_month(varname="PR", fname_prefix="pm", months_list=None, month_folde
     else:
         mean_field = fields[0] * 0
 
-    return mean_field, counter, {datetime(year, month, 15): np.asarray([field[LAND_MASK].mean() for field in fields]).mean()}
+    print(f"mean_field.shape={mean_field.shape} ")
+
+    return mean_field, counter, {
+        datetime(year, month, 15): np.asarray([field[LAND_MASK].mean() for field in fields]).mean()}
 
 
 if __name__ == '__main__':
---
+++ util/plot_utils.py
@@ -0,0 +1,170 @@
+from matplotlib.lines import Line2D
+from pathlib import Path
+
+__author__ = "huziy"
+__date__ = "$Aug 17, 2011 3:49:28 PM$"
+
+import numpy as np
+
+
+def draw_colorbar(fig, img, ax=None, boundaries=None, ticks=None):
+    """
+    Draw a nicely aligned colorbar to the axes ax
+    fig - containing figure
+    img - is the result returned from contourf or pcolormesh
+    """
+    from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
+
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", "5%", pad="3%")
+    cb = fig.colorbar(img, cax=cax, boundaries=boundaries, ticks=ticks)
+
+
+
+
+
+def get_closest_tick_value(nticks, lower_limit):
+    """
+    nticks - number of ticks in the colorbar
+    lower_limit - is the lower limit of the data to plot [0..1]
+    """
+
+    assert 0 <= lower_limit <= 1
+    d = 1.0 / float(nticks - 1.0)
+    assert d > 0
+
+    tick_value = 0
+    while tick_value <= 1:
+        if tick_value <= lower_limit <= tick_value + d:
+            if lower_limit - tick_value < tick_value + d - lower_limit:
+                return tick_value
+            else:
+                return tick_value + d
+        tick_value += d
+
+
+def apply_plot_params(font_size=20, width_pt=1000, aspect_ratio=1, height_cm=None, width_cm=None):
+    """
+    aspect_ratio = height / (width * golden_mean)
+    """
+    import pylab
+    import math
+
+    if width_pt is not None and width_cm is None:
+        inches_per_pt = 1.0 / 72.27  # Convert pt to inch
+        golden_mean = (math.sqrt(5.0) - 1.0) / 2.0  # Aesthetic ratio
+        fig_width = width_pt * inches_per_pt  # width in inches
+        fig_height = fig_width * golden_mean  # height in inches
+        fig_size = [fig_width, aspect_ratio * fig_height]
+    else:
+        inches_per_cm = 1.0 / 2.54
+        width_cm = 16.0 if width_cm is None else width_cm
+        height_cm = 23.0 if height_cm is None else height_cm
+        fig_size = [width_cm * inches_per_cm, height_cm * inches_per_cm]
+
+    params = {
+        'axes.labelsize': font_size,
+        "axes.titlesize": font_size * 1.5,
+        'font.size': font_size,
+        'legend.fontsize': font_size,
+        'xtick.labelsize': font_size,
+        'ytick.labelsize': font_size,
+        'figure.figsize': fig_size,
+    }
+
+    pylab.rcParams.update(params)
+
+
+def zoom_to_qc(plotter=None):
+    ymin, ymax = plotter.ylim()
+    plotter.ylim(ymin + 0.05 * (ymax - ymin), ymax * 0.25)
+
+    xmin, xmax = plotter.xlim()
+    plotter.xlim(xmin + (xmax - xmin) * 0.55, 0.72 * xmax)
+
+
+def draw_meridians_and_parallels(the_basemap, step_degrees=5.0, ax=None):
+    meridians = np.arange(-180, 180, step_degrees)
+    parallels = np.arange(-90, 90, step_degrees)
+    the_basemap.drawparallels(parallels, labels=[0, 0, 0, 0], fontsize=16, linewidth=0.25, ax=ax)
+    the_basemap.drawmeridians(meridians, labels=[0, 0, 0, 0], fontsize=16, linewidth=0.25, ax=ax)
+
+
+def get_ranges(x_interest, y_interest):
+    """
+    Get region of zoom for a given map
+    """
+    x_min, x_max = np.min(x_interest), np.max(x_interest)
+    y_min, y_max = np.min(y_interest), np.max(y_interest)
+    dx = 0.1 * (x_max - x_min)
+    dy = 0.1 * (y_max - y_min)
+    return x_min - dx, x_max + dx, y_min - dy, y_max + dy
+
+
+def save_to_shape_file(line_groups, folder_path="data/shape/derived_basins_qc", in_proj=None):
+    """
+    Save basin boundaries to a file
+    :param line_groups:
+    :param folder_path:
+    """
+    from fiona import collection
+    from shapely.geometry import LineString
+    from pyproj import Proj, transform
+    from fiona import crs
+
+    from shapely.geometry import mapping
+
+    lat_lon = crs.from_epsg(4326)
+    if in_proj is None:
+        in_proj = lat_lon
+
+    folder = Path(folder_path)
+    if not folder.exists():
+        folder.mkdir(parents=True)
+
+    shp = folder / "basin_boundaries_derived.shp"
+
+    schema = {"geometry": "LineString", "properties": {"id": "int"}}
+
+    print("in_proj = {}".format(in_proj))
+
+
+    with collection(str(shp), mode="w", driver="ESRI Shapefile", schema=schema, crs=lat_lon) as output:
+
+        p_in = Proj(in_proj)
+        p_out = Proj(output.crs)
+
+        for i, p in enumerate(line_groups):
+
+            print(p[0])
+            lines = [LineString(np.asarray(transform(p_in, p_out, *edge)).transpose()) for edge in p]
+            # poly = polygonize(lines)
+            for line in lines:
+                # print(line.wkt)
+                output.write({"properties": {"id": i}, "geometry": mapping(line)})
+
+
+def draw_upstream_area_bounds(the_ax, upstream_edges, **kwargs):
+    """
+
+    accepts all the parameters of the Line2D constructor
+
+    :param the_ax:
+    :param upstream_edges: list of lists of coordinates of edges to draw [[[(x1, x2), (y1, y2)], ...]]
+
+    :type the_ax: Axes
+
+    """
+    for p in upstream_edges:
+        for edge in p:
+            xs, ys = edge
+            the_ax.add_line(Line2D(xs, ys, **kwargs))
+
+
+
+
+
+if __name__ == "__main__":
+    print("Hello World")
+
+
